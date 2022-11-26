4a438e313793634123e8abd762c1f1e4dc6e43df
Sugeng Wahyudi <82944512+Gengsu07@users.noreply.github.com>
2022-11-26 23:13:18 +0700

    Update app.py

--- app.py
+++ app.py
@@ -17,35 +17,34 @@ def selenium(nip,password):
     firefoxOptions = Options()
     firefoxOptions.add_argument("--headless")
     service = Service(GeckoDriverManager().install())
-    profile = webdriver.FirefoxProfile()
-    # We set the coordinate of where we want to be.
-    profile.set_preference("geo.wifi.uri", 'data:application/json,{"location": params, "accuracy": 100.0}')
-    # This line is necessary to avoid to prompt for geolocation authorization.
-    profile.set_preference("geo.prompt.testing", True)
+#     profile = webdriver.FirefoxProfile()
+#     # We set the coordinate of where we want to be.
+#     profile.set_preference("geo.wifi.uri", 'data:application/json,{"location": params, "accuracy": 100.0}')
+#     # This line is necessary to avoid to prompt for geolocation authorization.
+#     profile.set_preference("geo.prompt.testing", True)
     driver = webdriver.Firefox(
-        profile,
         options=firefoxOptions,
         service=service,
     )
     
     driver.get('https://logbook.pajak.go.id/login')
-#     time.sleep(3)
-#     driver.find_element_by_id('nip').send_keys(nip)
-#     driver.find_element_by_id('password').send_keys(password)
-#     driver.find_element_by_id("m_login_signin_submit").click()
-#     time.sleep(2)
-#     driver.find_element_by_id('btnPresensi').click()
-#     time.sleep(2)
-#     driver.get('https://logbook.pajak.go.id/Presensi')
-#     time.sleep(1)
-#     data = pd.DataFrame(columns=['Hal',':','ket'])
-#     tabel =driver.find_elements_by_tag_name('tr')
-#     for baris in tabel[2:6]:
-#         row = baris.find_elements_by_tag_name('td')
-#         row_data = [x.text for x in row]
-#         data.loc[len(data)] = row_data
+    time.sleep(3)
+    driver.find_element_by_id('nip').send_keys(nip)
+    driver.find_element_by_id('password').send_keys(password)
+    driver.find_element_by_id("m_login_signin_submit").click()
+    time.sleep(2)
+    driver.find_element_by_id('btnPresensi').click()
+    time.sleep(2)
+    driver.get('https://logbook.pajak.go.id/Presensi')
+    time.sleep(1)
+    data = pd.DataFrame(columns=['Hal',':','ket'])
+    tabel =driver.find_elements_by_tag_name('tr')
+    for baris in tabel[2:6]:
+        row = baris.find_elements_by_tag_name('td')
+        row_data = [x.text for x in row]
+        data.loc[len(data)] = row_data
     
-    return driver.title
+    return data
     driver.close()
     driver.quit()
 
@@ -57,7 +56,7 @@ if __name__ == '__main__':
     if st.button('Absen Klik disini ygy'):
         with st.spinner('Sedang Mencoba...'):
             hasil = selenium(nip,password)
-            st.write(hasil)
+            st.table(hasil)
             st.code("credit by gengsu07")
 
 
